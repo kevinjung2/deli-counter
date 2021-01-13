8a58dab28fbcf15c7919db28d6e6a8254fb90b6c
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-13 14:56:57 -0500

    commit tree f42e0c1b99a376c80bc4d3fdb89ee1632728f211

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    line = "The line is currently: "
+    katz_deli.each.with_index(1) { |name, index| line += "#{index}. #{name} " }
+    puts line.strip
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  place = katz_deli.length
+  puts "Welcome, #{name}. You are number #{place} in line."
+end
+
+def now_serving(katz_deli_line)
+  if katz_deli_line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    serving = katz_deli_line.shift
+    puts "Currently serving #{serving}."
+  end
+end
